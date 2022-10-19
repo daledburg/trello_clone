06424768361c26116cc2359e135dc13049683ddf
Dale Dahlenburg <dale.19@icloud.com>
2022-10-19 22:38:15 +1000

    added auth

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
+app.config ['JSON_SORT_KEYS'] = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'password', 'is_admin')   
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -23,7 +40,9 @@ class Card(db.Model):
 class CardSchema(ma.Schema):
     class Meta:
         fields = ('id', 'title', 'description', 'date', 'status', 'priority')
+        ordered = True
 
+# Define Custom CLI (Terminal) Commands
 @app.cli.command('create')
 def create_db():
     db.create_all()
@@ -36,6 +55,18 @@ def drop_db():
 
 @app.cli.command('seed')
 def seed_db():
+    users = [
+        User(
+            email='admin@spam.com',
+            password=bcrypt.generate_password_hash('eggs').decode('utf-8'),
+            is_admin=True
+        ),
+        User(
+            name='John Cleese',
+            email='someone@spam.com',
+            password=bcrypt.generate_password_hash('12345').decode('utf-8')
+        )
+    ]
     cards = [
         Card(
             title = 'Start the project',
@@ -68,8 +99,28 @@ def seed_db():
     ]
 
     db.session.add_all(cards)
+    db.session.add_all(users)
     db.session.commit()
     print('Tables seeded')
+
+@app.route('/auth/register/', methods=['POST'])
+def auth_register():
+    try:
+        # Load the posted user info and parse the JSON
+        user_info = UserSchema().load(request.json)
+        # Create a new user model instance from the user_info
+        user = User(
+            email=user_info['email'],
+            password=bcrypt.generate_password_hash(user_info['password']).decode('utf-8'),
+            name=user_info['name']
+        )
+        # Add and commit user to DB
+        db.session.add(user)
+        db.session.commit()
+        # Respond to client
+        return UserSchema(exclude=['password']).dump(user), 201
+    except IntegrityError:
+        return {'error': 'Email already exists'}, 409
 
 @app.route('/cards/')
 def all_cards():
